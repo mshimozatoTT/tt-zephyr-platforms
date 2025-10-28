6e257cdaeee40c33cd403631cb6e220840334654
Mai Shimozato <mshimozato@tenstorrent.com>
2025-10-28 16:45:54 -0400

    drivers: dma: arc dma
    integrating the arc dma api into fw
    
    Signed-off-by: Mai Shimozato <mshimozato@tenstorrent.com>

--- lib/tenstorrent/bh_arc/pciesd.h
+++ lib/tenstorrent/bh_arc/pciesd.h
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "arc_dma.h"
 #include "pcie.h"
 
 #include <stdbool.h>
@@ -11,9 +10,6 @@
 #include <stdlib.h>
 
 #include <zephyr/toolchain.h>
-
-/* Verify prototype of ArcDmaTransfer, because it's used by libpciesd.a. */
-__unused static bool (*verify_ArcDmaTransfer)(const void *, void *, uint32_t) = ArcDmaTransfer;
 
 /* The functions below are implemented in tt_blackhole_libpciesd.a */
 PCIeInitStatus SerdesInit(uint8_t pcie_inst, PCIeDeviceType device_type,
